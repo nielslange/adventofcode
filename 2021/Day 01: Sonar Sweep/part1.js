e194e68285b3f2f8291dec8ac866047f30fb3a26
Niels Lange <info@nielslange.de>
2021-12-02 12:45:29 +0700

    Convert code to function

--- 2021/Day 01: Sonar Sweep/part1.js
+++ 2021/Day 01: Sonar Sweep/part1.js
@@ -1,9 +1,16 @@
 const fs = require( 'fs' );
-const data = fs.readFileSync( 'puzzle-input.txt', 'utf8' ).split( '\n' );
-let counter = 0;
+const data = fs.readFileSync( 'puzzle.txt', 'utf8' ).split( '\n' );
 
-for ( let i = 1; i < data.length; i++ ) {
-	if ( parseInt( data[ i ] ) > parseInt( data[ i - 1 ] ) ) counter++;
+function getSimpleCount( array ) {
+	let count = 0;
+
+	for ( let i = 1; i < array.length; i++ ) {
+		if ( parseInt( array[ i ] ) > parseInt( array[ i - 1 ] ) ) count++;
+	}
+
+	return count;
 }
 
-console.info( counter );
+console.time();
+console.info( getSimpleCount( data ) );
+console.timeEnd();
--- 2021/Day 01: Sonar Sweep/part2.js
+++ 2021/Day 01: Sonar Sweep/part2.js
@@ -1,17 +1,24 @@
 const fs = require( 'fs' );
-const data = fs.readFileSync( 'puzzle-input.txt', 'utf8' ).split( '\n' );
-let counter = 0;
+const data = fs.readFileSync( 'puzzle.txt', 'utf8' ).split( '\n' );
 
-for ( let i = 2; i < data.length - 1; i++ ) {
-	const first =
-		parseInt( data[ i - 2 ] ) +
-		parseInt( data[ i - 1 ] ) +
-		parseInt( data[ i ] );
-	const second =
-		parseInt( data[ i - 1 ] ) +
-		parseInt( data[ i ] ) +
-		parseInt( data[ i + 1 ] );
-	if ( second > first ) counter++;
+function getAdvancedCount( array ) {
+	let count = 0;
+
+	for ( let i = 3; i < array.length; i++ ) {
+		const first =
+			parseInt( array[ i - 3 ] ) +
+			parseInt( array[ i - 2 ] ) +
+			parseInt( array[ i - 1 ] );
+		const second =
+			parseInt( array[ i - 2 ] ) +
+			parseInt( array[ i - 1 ] ) +
+			parseInt( array[ i ] );
+		if ( second > first ) count++;
+	}
+
+	return count;
 }
 
-console.info( counter );
+console.time();
+console.info( getAdvancedCount( data ) );
+console.timeEnd();
